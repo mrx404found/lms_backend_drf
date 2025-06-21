6e3d4b640db70885d0a781d9c18ae01d0d018eee
Mr. X <click55444111@outlook.com>
2025-06-22 00:39:58 +0600

    Update settings.py

--- lms_backend/settings.py
+++ lms_backend/settings.py
@@ -19,6 +19,8 @@ ALLOWED_HOSTS = ["*"]  # Change this to your domain in production
 CORS_ALLOWED_ORIGINS = [
     "https://lms-backend-drf-ostad.onrender.com",
     "https://lms-frontend-zeta-three.vercel.app",
+    "http://localhost:5173",
+    "http://127.0.0.1:8000",
 ]
 
 
