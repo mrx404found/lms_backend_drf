15a726c2d352e14a8a92bcc69ddc29ca61306999
Mr. X <click55444111@outlook.com>
2025-06-22 09:20:24 +0600

    Update settings.py

--- lms_backend/settings.py
+++ lms_backend/settings.py
@@ -137,7 +137,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
