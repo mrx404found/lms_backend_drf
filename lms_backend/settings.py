fdb1787dc3c61576412f8196b935d71b45886f49
Mr. X <click55444111@outlook.com>
2025-06-22 09:27:57 +0600

    Update settings.py

--- lms_backend/settings.py
+++ lms_backend/settings.py
@@ -140,6 +140,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
